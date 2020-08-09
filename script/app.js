880663a857be1c89f81a026c12d11b85af12784b
ashwanizsktm <ashwanizskt@gmail.com>
2020-08-09 17:15:09 +0530

    gsap added

--- script/app.js
+++ script/app.js
@@ -1,3 +1,23 @@
+// my profile animation
+const words = ["Ashwani.", "a Web Developer.", "passionate in developing user interfaces."];
+
+let cursor = gsap.to('.cursor', {opacity: 0, ease:"power2.inOut", repeat: -1});
+
+let boxTl = gsap.timeline();
+boxTl.to('.box', {duration: 1, width: "11vw", 
+delay: 0.5, ease: "power4.inOut"})
+.from('.hi', {duration: 1, y: "7vw", ease: "power3.out", onComplete:()=>masterTl.play()})
+.to('.box', {duration:1, ease: "elastic.out"});
+
+let masterTl = gsap.timeline({repeat: -1,}).pause();
+
+words.forEach(word => {
+  let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1})
+  tl.to('.text', {duration: 1, text: word})
+  masterTl.add(tl);
+})
+
+
 $(function () {
   
   var link = $('#navbar a.nav-link-item');
